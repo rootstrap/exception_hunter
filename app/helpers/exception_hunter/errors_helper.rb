09e7145d1fea9ffb3d9020d804a8db2805705ed9
Bruno Vezoli <c-bruno.vezoli@masterclass.com>
2020-04-29 14:50:29 -0300

    Fix comments

--- app/helpers/exception_hunter/errors_helper.rb
+++
@@ -1,4 +0,0 @@
-module ExceptionHunter
-  module ErrorsHelper
-  end
-end
--- spec/requests/exception_hunter/errors_request_spec.rb
+++ spec/requests/exception_hunter/errors_request_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "Errors", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/errors/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+describe 'Errors', type: :request do
+  pending 'to be added'
 end
