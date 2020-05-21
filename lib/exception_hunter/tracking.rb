d04d99860ac48fb24566846986cb24a17c29b25a
Bruno Vezoli <c-bruno.vezoli@masterclass.com>
2020-05-20 21:59:26 -0300

    Allow tracking exceptions manually

---
+++ lib/exception_hunter/tracking.rb
@@ -0,0 +1,15 @@
+module ExceptionHunter
+  module Tracking
+    def track(exception, custom_data: {}, user: nil)
+      ErrorCreator.call(
+        class_name: exception.class.to_s,
+        message: exception.message,
+        backtrace: exception.backtrace,
+        custom_data: custom_data,
+        user: user
+      )
+
+      nil
+    end
+  end
+end
